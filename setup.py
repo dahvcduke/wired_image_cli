d0957c2f64a7927fe4ee37e07e93a7df8eef4f4a
Matt Kenney <mk365@duke.edu>
2018-08-31 13:41:48 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup 
 
 setup(
-    name='wired_cli',
+    name='wired_image_cli',
     version='1.4',
-    py_modules=['wired_cli'],
+    py_modules=['wired_image_cli'],
     include_package_data=True,
     install_requires=[
         'click',
         'tqdm',
-        'tabulate',
+        'cv2',
         'gensim',
         'pandas',
         'emoji',
-        'spacy',
-        'newspaper3k',
+        'keras',
+        'scipy',
         'PyInquirer',
-        'pyfiglet'
+        'pyfiglet',
+        'tensorflow',
+        'numpy',
+        'pickle',
     ],
     entry_points='''
         [console_scripts]
-        wired_cli=wired_cli:cli
+        wired_image_cli=wired_image_clis:cli
     ''',
 )
